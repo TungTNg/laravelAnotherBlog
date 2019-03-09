3044a305af9fc9e5558ce1ba8aaae1934bba41a6
Tung Nguyen <mecer80@gmail.com>
2019-03-09 22:19:55 +0000

    User Authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href={{ asset('public/css/app.css') }}>
-        <title>{{ config('app.name', 'Another Laravel Blog') }}</title>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
         @include('inc.navbar')
         <div class="container">
             @include('inc.messages')
             @yield('content')
         </div>
-        
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('public/js/app.js') }}"></script>
     <script src="/laravelAnotherBlog/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
-    </body>
+</body>
 </html>
